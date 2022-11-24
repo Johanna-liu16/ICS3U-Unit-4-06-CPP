84c1d92b8b047d89409e29740f34519c1ba411e0
Johanna Liu <johanna.liu@mths.ca>
2022-11-24 23:51:49 +0000

    fixed

--- rgb.cpp
+++ rgb.cpp
@@ -18,8 +18,8 @@ int main() {
     for (int loopCounter1 = 0; loopCounter1 < 255; loopCounter1++) {
         for (int loopCounter2 = 0; loopCounter2 < 255; loopCounter2++) {
             for (int loopCounter3 = 0; loopCounter3 < 255; loopCounter3++) {
-                std::cout << "R " << loopCounter1 << "G " << loopCounter2 << " B" 
-                << loopCounter3 << std::endl;
+                std::cout << "R " << loopCounter1 << "G "
+                << loopCounter2 << " B" << loopCounter3 << std::endl;
             }
         }
     }
